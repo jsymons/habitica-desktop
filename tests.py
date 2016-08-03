738d2d7f874c4d91cbceeff70011a50689941eb3
Jason Symons <jason.symons@gmail.com>
2016-08-03 07:52:44 -0400

    Retrieve and store user profile

--- tests.py
+++ tests.py
@@ -3,21 +3,35 @@ import unittest
 from habitica_api import user
 
 
+loginfile = open('test_credentials')
+username = loginfile.readline().strip()
+password = loginfile.readline().strip()
+loginfile.close()
+
 class TestAPILogin(unittest.TestCase):
 
 	def setUp(self):
 		self.user = user.NewLogin()
-		loginfile = open('test_credentials')
-		self.username = loginfile.readline().strip()
-		self.password = loginfile.readline().strip()
-		loginfile.close()
-
+		
 
 	def test_not_logged_in(self):
 		self.assertFalse(self.user.login_status)
 
 	def test_login(self):
-		self.user.login(self.username,self.password)
+		self.user.login(username,password)
 		self.assertTrue(self.user.login_status)
 
 
+class TestUserProfile(unittest.TestCase):
+
+	def setUp(self):
+		self.user = user.NewLogin()
+		self.user.login(username,password)
+		
+
+	def test_status_update(self):
+		self.assertTrue(self.user.update_status())
+
+
+
+		
