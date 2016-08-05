eeef70efa68dc5d47a50e93d5f5a41f5db37c62d
Jason Symons <jason.symons@gmail.com>
2016-08-05 12:14:25 -0400

    Advanced habit creation

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 from habitica_api import user
-
+from habitica_api import task
 
 loginfile = open('test_credentials')
 username = loginfile.readline().strip()
@@ -58,9 +58,20 @@ class TestTasks(unittest.TestCase):
 		self.assertTrue(test_task_name in [habit.title for habit in self.user.habits])
 
 	def test_add_habit(self):
-		test_task_name = "Test creation habit"
-		self.user.add_habit(test_task_name)
-		self.assertTrue(test_task_name in [habit.title for habit in self.user.habits])
+		test_task = {}
+		test_task['title'] = "Test creation habit"
+		test_task['notes'] = "Test habit notes"
+		test_task['up'] = True
+		test_task['down'] = False
+		test_task['difficulty'] = 1.5
+
+		self.user.add_habit(**test_task)
+		self.assertTrue(test_task['title'] in [habit.title for habit in self.user.habits])
+		for habit in [habit for habit in self.user.habits if habit.title == test_task['title']]:
+			self.assertTrue(habit.notes == test_task['notes'], 'Notes do not match')
+			self.assertTrue(habit.up == test_task['up'], 'Up is set to false')
+			self.assertTrue(habit.down == test_task['down'], 'Down is set to true')
+			self.assertTrue(habit.difficulty == test_task['difficulty'], 'Difficulty does not match')
 
 	def test_delete_habit(self):
 		test_task_name = "Test deletion habit"
